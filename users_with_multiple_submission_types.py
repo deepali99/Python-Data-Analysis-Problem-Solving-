37de4043929bde23a21a809095da2865e9a5ebcb
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-03-11 16:45:11 -0700

    Update users_with_multiple_submission_types.py

--- users_with_multiple_submission_types.py
+++ users_with_multiple_submission_types.py
@@ -1,7 +1,7 @@
-/*
+'''
 https://platform.stratascratch.com/coding/2002-submission-types?code_type=2
 Write a query that returns the user ID of all users that have created at least one ‘Refinance’ submission and at least one ‘InSchool’ submission.
-*/
+'''
 
 import pandas as pd 
 result_1 = loans[loans['type'] == 'Refinance']
